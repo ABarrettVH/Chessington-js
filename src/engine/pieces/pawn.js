72572b126795da3176007acdc47b312bf1814ae9
Alice Barrett <alice.barrett@softwire.com>
2025-04-29 14:52:14 +0100

    Pawns can only move into available spaces

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -11,19 +11,43 @@ export default class Pawn extends Piece {
         let location = board.findPiece(this)
 
         if (this.player === Player.WHITE){
+            let blockingPiece1 = {row:location.row+1,col:location.col};
+            let blockingPiece2 = {row:location.row+2,col:location.col};
             if (location.row === 1) {
-                return [Square.at(location.row + 1, location.col),Square.at(location.row + 2, location.col)]
+                if (board.getPiece(blockingPiece1)){
+                    return [];
+                } else if (board.getPiece(blockingPiece2)) {
+                    return [Square.at(location.row + 1, location.col)];
+                } else {
+                    return [Square.at(location.row + 2, location.col),Square.at(location.row + 1, location.col)];
+                }
             }
             else {
-                return [Square.at(location.row + 1, location.col)]
+                if (board.getPiece(blockingPiece1)) {
+                    return [];
+                } else {
+                    return [Square.at(location.row + 1, location.col)];
+                }
             }
         }
         else {
+            let blockingPiece1 = {row:location.row-1,col:location.col};
+            let blockingPiece2 = {row:location.row-2,col:location.col};
             if (location.row === 6) {
-                return [Square.at(location.row - 2, location.col),Square.at(location.row - 1, location.col)]
+                if (board.getPiece(blockingPiece1)){
+                    return [];
+                } else if (board.getPiece(blockingPiece2)) {
+                    return [Square.at(location.row - 1, location.col)];
+                } else {
+                    return [Square.at(location.row - 2, location.col),Square.at(location.row - 1, location.col)];
+                }
             }
             else {
-               return [Square.at(location.row - 1, location.col)]
+                if (board.getPiece(blockingPiece1)) {
+                    return [];
+                } else { 
+                    return [Square.at(location.row - 1, location.col)];
+                }
             }
         }
     }
