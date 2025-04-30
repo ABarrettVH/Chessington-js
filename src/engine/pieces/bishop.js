03e53ea216403b61d6ae8210573e599161ddae63
Alice Barrett <alice.barrett@softwire.com>
2025-04-30 22:05:53 +0100

    Bishops can only move diagonally

--- src/engine/pieces/bishop.js
+++ src/engine/pieces/bishop.js
@@ -1,3 +1,5 @@
+import Player from '../player';
+import Square from '../square';
 import Piece from './piece';
 
 export default class Bishop extends Piece {
@@ -6,6 +8,47 @@ export default class Bishop extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        const location = board.findPiece(this);
+        let availableMoves=[];
+        let friendlyBlockingPieces =[];
+        let friendlyColour;
+        let unfriendlyColour;
+        let availableSquare;
+
+        if (this.player === Player.WHITE) {
+            friendlyColour = Player.WHITE;
+            unfriendlyColour = Player.BLACK;
+        }
+        else {
+            friendlyColour = Player.BLACK;
+            unfriendlyColour = Player.WHITE;
+        }
+
+        for (let i = 1; i <8; i++) {
+            availableSquare = Square.at(location.row+i,location.col+i);
+            if (availableSquare.col < 8 && availableSquare.row < 8) {
+            availableMoves.push(availableSquare);
+            }
+        }
+        for (let i = 1; i <8; i++) {
+            availableSquare = Square.at(location.row-i,location.col+i);
+            if (availableSquare.col < 8 && availableSquare.row >= 0) {
+            availableMoves.push(availableSquare);
+            }
+        }
+        for (let i = 1; i <8; i++) {
+            availableSquare = Square.at(location.row+i,location.col-i);
+            if (availableSquare.col >= 0 && availableSquare.row < 8) {
+            availableMoves.push(availableSquare);
+            }
+        }
+        for (let i = 1; i <8; i++) {
+            availableSquare = Square.at(location.row-i,location.col-i);
+            if (availableSquare.col >= 0 && availableSquare.row >= 0) {
+            availableMoves.push(availableSquare);
+            }
+        }
+        console.log(availableMoves);
+        return availableMoves;
     }
 }
