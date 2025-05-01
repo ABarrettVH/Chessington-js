8dc841e4a95aad214d84ff39a25aecae79513d1e
Alice Barrett <alice.barrett@softwire.com>
2025-05-01 12:07:10 +0100

    King can move one in all directions and take pieces, except kings

--- src/engine/pieces/king.js
+++ src/engine/pieces/king.js
@@ -1,4 +1,6 @@
-import Piece from './piece';
+import Player from '../player';
+import Square from '../square';
+import Piece from './piece'
 
 export default class King extends Piece {
     constructor(player) {
@@ -6,6 +8,56 @@ export default class King extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+
+
+        let location = board.findPiece(this)
+        let availableMoves=[];
+        let unfriendlyColour;
+        let availableSquare;
+        let rowStart = location.row-1;
+        let rowEnd = location.row+1;
+        let colStart = location.col-1;
+        let colEnd = location.col+1;
+
+        if (this.player === Player.WHITE) {
+            unfriendlyColour = Player.BLACK;
+        }
+        else {
+            unfriendlyColour = Player.WHITE;
+        }
+
+        if (location.col === 7){
+            colEnd = location.col;
+        };
+        if (location.col === 0){
+            colStart = location.col;
+        }
+        if (location.row === 7){
+            rowEnd = location.row;
+        }
+        if (location.row === 0){
+            rowStart = location.row;
+        }
+
+        for (let i=rowStart; i<=rowEnd; i++){
+            for (let j=colStart; j<=colEnd;j++){
+                availableSquare = Square.at(i,j);
+                // console.log(availableSquare);
+                if (!(i === location.row && j === location.col)){
+                    if (!(board.getPiece(availableSquare))){
+                        availableMoves.push(availableSquare);
+                    }
+                    else if (board.getPiece(availableSquare).player === unfriendlyColour && board.getPiece(availableSquare).constructor.name !== "King") {
+                        availableMoves.push(availableSquare);
+                    }
+                    else {
+                        break;
+                    }
+                }
+            }
+        }
+
+
+        return availableMoves; 
     }
 }
--- tests/engine/pieces/king.spec.js
+++ tests/engine/pieces/king.spec.js
@@ -1,5 +1,111 @@
+import king from '../../../src/engine/pieces/king';
+import 'chai/register-should';
+import Rook from '../../../src/engine/pieces/rook';
+import Pawn from '../../../src/engine/pieces/pawn';
 import King from '../../../src/engine/pieces/king';
+import Board from '../../../src/engine/board';
+import Player from '../../../src/engine/player';
+import Square from '../../../src/engine/square';
+
 
 describe('King', () => {
 
+        let board;
+        beforeEach(() => board = new Board());
+    
+        it('can move laterally', () => {
+            const king = new King(Player.WHITE);
+            board.setPiece(Square.at(5, 0), king);
+    
+            const moves = king.getAvailableMoves(board);
+    
+            const expectedMoves = [
+                // Horizontal
+                Square.at(5, 1),
+                // Vertical
+                Square.at(6, 0), Square.at(4, 0)
+            ];
+    
+            moves.should.deep.include.members(expectedMoves);
+        });
+
+        it('can move diagonally', () => {
+            const king = new King(Player.WHITE);
+            board.setPiece(Square.at(5,0), king);
+    
+            const moves = king.getAvailableMoves(board);
+    
+            const expectedMoves = [
+                // forwards diagonal /
+                Square.at(6,1), 
+                // backwards diagonal \
+                Square.at(4,1),
+            ];
+    
+            moves.should.deep.include.members(expectedMoves);
+        });
+
+        it('cannot make any other moves', () => {
+            const king = new King(Player.WHITE);
+            board.setPiece(Square.at(1,7), king);
+    
+            const moves = king.getAvailableMoves(board);
+    
+            moves.should.have.length(5);
+        });
+
+        it('cannot move through friendly pieces', () => {
+            const king = new King(Player.WHITE);
+            const friendlyPiece = new Pawn(Player.WHITE);
+            board.setPiece(Square.at(4,3), king);
+            board.setPiece(Square.at(3,2), friendlyPiece);
+    
+            const moves = king.getAvailableMoves(board);
+    
+            moves.should.not.deep.include(Square.at(3,2),Square.at(2,1),Square.at(1,0));
+        });
+    
+        it('cannot move through opposing pieces', () => {
+            const king = new King(Player.BLACK);
+            const opposingPiece = new Pawn(Player.WHITE);
+            board.setPiece(Square.at(4,3), king);
+            board.setPiece(Square.at(4,4), opposingPiece);
+    
+            const moves = king.getAvailableMoves(board);
+    
+            moves.should.not.deep.include(Square.at(4,5),Square.at(4,6),Square.at(4,7));
+        });
+
+        it('can take opposing pieces', () => {
+            const king = new King(Player.WHITE);
+            const opposingPiece = new Pawn(Player.BLACK);
+            board.setPiece(Square.at(3,5), king);
+            board.setPiece(Square.at(4,4), opposingPiece);
+    
+            const moves = king.getAvailableMoves(board);
+    
+            moves.should.deep.include(Square.at(4,4));
+        });
+
+        it('cannot take friendly pieces', () => {
+            const king = new King(Player.BLACK);
+            const friendlyPiece = new Pawn(Player.BLACK);
+            board.setPiece(Square.at(4, 1), king);
+            board.setPiece(Square.at(3, 2), friendlyPiece);
+    
+            const moves = king.getAvailableMoves(board);
+    
+            moves.should.not.deep.include(Square.at(3, 2));
+        });
+
+        it('cannot take the opposing king', () => {
+            const king = new King(Player.BLACK);
+            const opposingKing = new King(Player.WHITE);
+            board.setPiece(Square.at(2,4), king);
+            board.setPiece(Square.at(1,3), opposingKing);
+    
+            const moves = king.getAvailableMoves(board);
+    
+            moves.should.not.deep.include(Square.at(1,3));
+        });
 });
